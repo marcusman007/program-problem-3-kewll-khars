5ef5125bf452bd9d5e75925a47c8ba44f85dcb41
marcusman007 <31863992+marcusman007@users.noreply.github.com>
2017-10-16 13:59:26 -0500

    Asignment 4
    I created a loop for it to run 30 times

--- Three_Digit_Ascend_Descend_Selection/KyleAustin_MaazKamal_JustinVanNimwegen.cpp
+++ Three_Digit_Ascend_Descend_Selection/KyleAustin_MaazKamal_JustinVanNimwegen.cpp
@@ -1,47 +1,40 @@
-/* 
-Maaz Kamal, Kyle Austin, Justin Van Nimwegen, Yezen Higazin - 4
-
-Assignment Name : Three_Digit_Ascend_Descend_Selection
-
-Taking in data and tranfering into 'if' statements
-
+/*
+-Maaz Kamal, Kyle Austin, Justin Van Nimwegen, Yezen Higazin - 4
+	Edited By: Marcus Basta
+Assignment: Three Digit Ascend Decend Selection
 */
-
-//Libraries
-#include <iostream> 
+#include <iostream>
 #include <conio.h>
 
-//namespaces
 using namespace std;
 
-//functions
-void pause() {
-	cout << "Press any key to continue . . .";
-	while (!_kbhit());
-	_getch();
-	cout << '/n';
+
+ void pause() {
+	 cout << "press any key to continue. . . ";
+	 while (!_kbhit());
+	 _getch();
+	 cout << '/n';
 }
 
-void main() {
-	int x;
-	cout << "Choose a 3 Digit #" << endl;
-	cin >> x;
-	cout << "You Chose " << x << endl;
+ void main() {
+	 int x;
 
-	int A = x / 100;
-	int B = x % 100 / 10;
-	int C = x % 10;
-
-	if ((A > B) && (B > C)) {
-		cout << "Decending" << endl;
-
-	}
-	else if ((A < B) && (B < C)) {
-		cout << "Acending" << endl;
-		}
-	else {
-		cout << "Neither" << endl;
-	}
-
-	pause();
-}
+	 for (int i = 1; i <= 30; i++) {
+		 cout << i << endl;
+		 cout << "Choose a 3 Digit #" << endl;
+		 cin >> x;
+		 int A = x / 100;
+		 int B = x % 100 / 10;
+		 int C = x % 10;
+		 cout << "Choose wiseley " << x << endl;
+		 if ((A > B) && (B > C)) {
+			 cout << "decending" << endl;
+		 }
+		 else if ((A < B) && (B < C)) {
+			 cout << "acending" << endl;
+		 }
+		 else {
+			 cout << "neither" << endl;
+		 }
+	 }
+ }
